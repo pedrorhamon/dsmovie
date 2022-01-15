09c9e4be0957e8856867e5e2f919c8a774949bf3
pedrorhamon <pedrorhamon16@gmail.com>
2022-01-14 21:10:54 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,10 +1,29 @@
+import MovieCard from "components/MovieCard";
+
 import Pagination from "components/Pagination";
 import './styles.css';
 
-function Listing () {
+function Listing() {
     return (
-        <h1><Pagination/></h1>
-    )
+        <>
+            <Pagination />
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-x1-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-x1-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-x1-3 mb-3">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+
+        </>
+    );
 }
 
 export default Listing;
