98cb413d1f9f3af3bd18e650a8f061224d6a0b19
pedrorhamon <pedrorhamon16@gmail.com>
2022-01-16 20:18:38 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,19 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 
 import Pagination from "components/Pagination";
+import { BASE_URL } from "utils/requests";
 import './styles.css';
 
 function Listing() {
+
+    //FORMA ERRADA
+    axios.get(`${BASE_URL}/movies?size=12&page-0`)
+    .then(response =>{
+        console.log(response.data);
+        
+    });
+
     return (
         <>
             <Pagination />
@@ -21,7 +31,6 @@ function Listing() {
                     </div>
                 </div>
             </div>
-
         </>
     );
 }
